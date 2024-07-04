e5b828b1955106bb3de2776d6a6c3f69e34b8f96
Therese-Claire <awonguagabi@gmail.com>
2024-07-05 00:22:53 +0100

    type-annotated function concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+"""Task 1 module
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """type-annotated function concat that takes a string str1 and
+    a string str2 as arguments and returns a concatenated string.
+
+    Args:
+        str1 (str): first strimg argument.
+        str2 (str): second string argument.
+
+    Retuns:
+        str: concatenated string.
+    """
+    return str1 + str2
