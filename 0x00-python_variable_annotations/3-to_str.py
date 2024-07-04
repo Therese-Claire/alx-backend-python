670725197322183bbd80d3e06b4527924a17e12e
Therese-Claire <awonguagabi@gmail.com>
2024-07-05 00:51:14 +0100

    type-annotated to_str()

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Task 3 module
+"""
+
+
+def to_str(n: float) -> str:
+    """type-annotated function to_str that takes a float n
+    as argument and returns the string representation of the float.
+
+    Args:
+        n (float): string argument to convert to int
+
+    Returns:
+        str: int representation of n.
+    """
+    return str(n)
