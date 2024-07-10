1112aea6af9034b5ef3b01da250772229a467363
Therese-Claire <awonguagabi@gmail.com>
2024-07-10 02:51:06 +0100

    Coroutine loop

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -5,6 +5,12 @@ import random
 
 
 async def async_generator():
+    """Coroutine that generates random numbers between 0 and 10, one number
+    at a time.
+
+    Yields:
+        Generator[float, None, None]:  A random number between 0 and 10.
+    """
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
